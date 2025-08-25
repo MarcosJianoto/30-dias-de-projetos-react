f623097033dbab295c5438288fab46b540687297
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-08-25 18:50:09 -0300

    init calculator

--- calculadora-simples/src/App.jsx
+++ calculadora-simples/src/App.jsx
@@ -9,25 +9,35 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Calculadora React</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh', width: '25vh', flexDirection: 'column', border: '1px solid red' }}>
+
+        <div>
+
+          <div style={{ marginBottom: '50px' }}>
+            <h3>Primeiro numero: </h3>
+            <input type="number" />
+          </div>
+
+          <div>
+            <h3>Segundo numero: </h3>
+            <input type="number" />
+          </div>
+
+          <div>
+            <select name="tipo de conta" id="1"></select>
+          </div>
+
+          <div>
+            <p>Resultado: { }</p>
+          </div>
+
+        </div>
+
+
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
