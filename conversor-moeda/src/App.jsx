2db3cfa96864fc27cc9a9d709bc0662cd2f6849f
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-08-26 19:59:54 -0300

    conversos 1 pronto

--- conversor-moeda/src/App.jsx
+++ conversor-moeda/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [brl, setBrl] = useState(0)
+  const [usd, setUsd] = useState(0)
+  let [cotacao, setCotacao] = useState(0)
+
+
+  useEffect(() => {
+    fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+      .then(response => response.json())
+      .then(data => {
+        const valor = Number(data.USDBRL.bid);
+        setCotacao(valor);
+
+      }).catch(error => {
+        console.error('Erro ao buscar a cotação:', error);
+      });
+  }, []);
+
+  const handleConvert = () => {
+    setBrl(usd * cotacao);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className="min-h-screen bg-yellow-50 p-4">
+      <div className='mx-auto p0 text-left border border-black w-90 mt-10 rounded-lg bg-white'>
+        <div className='p-4'>
+          <h1 className='text-2xl font-bold'>Conversor de Moeda</h1>
+          <h4>Cotação Atual: {cotacao}  </h4>
+          <h2 className='font-bold pt-1' >USD:</h2>
+          <div className='text-center' >
+            <input type="number" className='border border-gray-400 rounded-sm w-full p-2' onChange={(e) => setUsd(Number(e.target.value))} />
+          </div>
+          <h2 className='font-bold mt-4 mb-1' >BRL:</h2>
+          {usd * cotacao}
+        </div>
+
+        <div>
+          <hr className='border-gray-200' />
+        </div>
+
+        <div className='p-4 text-center'>
+          <button className='bg-green-500 text-[#fff] font-bold px-31 py-2 rounded' >Converter</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
-export default App
+export default App 
