61689b74c11028f93ec39c9a32d40ff162aa4e79
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-08-30 18:30:42 -0300

    study, add new object in arr

--- cronometro-tailwind/src/components/Points.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Points = ({ points }) => {
-    return (
-        <div className=' flex text-align-center h-40 text-9xl text-white'>{points}</div>
-    )
-}
-
-export default Points
