7144fbc4749c33b71ea9f637e59213e33df59d86
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-04 20:51:04 -0300

    converter celcius

---
+++ cronometro-tailwind copy/src/App.jsx
@@ -0,0 +1,25 @@
+
+import React, { useEffect, useState } from 'react';
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prev => prev + 1)
+    }, 1000)
+    return () => clearInterval(interval);
+  }, [])
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-800 text-white'>
+      <div>Contador automático</div>
+      <h1>{count}</h1>
+
+    </div >
+  )
+}
+
+export default App
--- cronometro-tailwind/src/App.jsx
+++ cronometro-tailwind/src/App.jsx
@@ -1,22 +1,40 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 
 function App() {
+  //  8. Converter Celsius para Fahrenheit
+  // Descrição: Crie um input para digitar graus Celsius e mostre o valor convertido para Fahrenheit.
+  // Objetivo: Trabalhar com eventos, useState e fórmulas simples.
+  // Resultado esperado: O valor convertido é atualizado enquanto você digita.
 
   const [count, setCount] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(0);
 
+  // useEffect(() => {
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount(prev => prev + 1)
-    }, 1000)
-    return () => clearInterval(interval);
-  }, [])
+  //   setFahrenheit((count * (9 / 5) + 32))
+
+  // }, [count])
+
+  const handleCalc = (count) => {
+
+    return setFahrenheit((count * (9 / 5) + 32))
+
+  }
+
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen bg-gray-800 text-white'>
-      <div>Contador automático</div>
-      <h1>{count}</h1>
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-800 text-white mx-auto'>
+
+      <h1>Fahrenheit -&gt; Celcius</h1>
+
+      <h1 className='mt-5'>Celcius</h1>
+      <input type="number" placeholder='Digite o valor' className='border border-white/30 w-50' onChange={(e) => setCount(Number(e.target.value))} />
+
+      <h1 className='mt-5'>Fahrenheit</h1>
+      <p className='border border-white/50 w-50 items-center justify-center'>{fahrenheit}</p>
+
+      <button className='border border-white/30 mt-10 rounded-lg px-3 cursor-pointer hover:bg-sky-600 transition' onClick={() => handleCalc(count)}>Converter em Fahrenheit</button>
 
     </div >
   )
