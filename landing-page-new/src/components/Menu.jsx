747c2a696667969dc353ce98319caf7038845d8f
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-09 21:24:22 -0300

    new portfolio wip

--- landing-page-new/src/components/Menu.jsx
+++ landing-page-new/src/components/Menu.jsx
@@ -1,16 +1,18 @@
 const Menu = ({ one, two, three }) => {
 
     const stringOrImage = (item) => {
+
+        console.log(item)
         if (typeof item === 'string' && (item.endsWith('.png') || item.endsWith('.jpg') || item.endsWith('.jpeg') || item.endsWith('.svg'))) {
-            if (item.charAt(0) === 'o') {
+            if (item.endsWith('github.png')) {
                 return <a href="https://github.com/MarcosJianoto"><img src={item} alt="icon" className="h-10 w-10" /></a>
-            } else if (item.charAt(0) === 't') {
+            } else if (item.endsWith('linkedin.png')) {
                 return <a href="https://www.linkedin.com/in/marcosjianoto/"><img src={item} alt="icon" className="h-8 w-8" /></a>
-            } else {
+            } else if (item.endsWith('instagram.png')) {
                 return <a href="https://www.instagram.com/marcosjianoto/"><img src={item} alt="icon" className="h-9 w-9" /></a>
             }
         } else {
-            return <h2>{item}</h2>;
+            return <h3>{item}</h3>;
         }
     }
 
