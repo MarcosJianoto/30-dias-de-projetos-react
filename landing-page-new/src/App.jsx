696456522a9bdc515a4ebe919b45a325edf777f9
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-13 18:02:49 -0300

    okay

--- landing-page-new/src/App.jsx
+++ landing-page-new/src/App.jsx
@@ -7,27 +7,39 @@ function App() {
 
   return (
 
-    <div className="flex flex-col min-h-screen bg-gray-950" >
+    <div className="bg-gray-950" >
 
-      <div className='flex-1 flex flex-col justify-center items-center text-white'>
-        <div className=' text-sm items-end'>i am</div>
-        <div className='font-black text-7xl my-5'>MARCOS JIANOTO</div>
-        <div className="flex flex-col text-sm mt-2 items-end">
-          <div>full stack</div>
-          <div>developer</div>
+      <div className='flex flex-col h-screen border border-white justify-center'>
+        <div className='flex flex-col justify-center items-center text-white my-80'>
+          <div className='text-sm'>i am</div>
+          <div className='font-black text-7xl my-5'>MARCOS JIANOTO</div>
+          <div className="flex flex-col text-sm mt-2 items-end">
+            <div>full stack</div>
+            <div>developer</div>
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-3 justify-center items-start ml-10 text-white pb-5'>
+          <a href="#">
+            <img src={Github} alt="ícone do github" className='h-5' />
+          </a>
+          <a href="#">
+            <img src={Linkedin} alt="ícone do linkedin" className='h-5' />
+          </a>
+          <a href="#">
+            <img src={Facebook} alt="ícone do facebook" className='h-4' />
+          </a>
         </div>
       </div>
 
-      <div className='flex flex-col gap-3 justify-center items-start ml-10 text-white pb-5'>
-        <a href="#">
-          <img src={Github} alt="ícone do github" className='h-5' />
-        </a>
-        <a href="#">
-          <img src={Linkedin} alt="ícone do linkedin" className='h-5' />
-        </a>
-        <a href="#">
-          <img src={Facebook} alt="ícone do facebook" className='h-4' />
-        </a>
+
+
+      <div>
+        <div className='text-white text-center text-xs pb-3'>Designed by <a href="https://www.freepik.com/free-vector/gradient-technology-background_5389761.htm#query=technology%20background&position=0&from_view=search&track=sph" className='underline'>Freepik</a></div>
+      </div>
+
+      <div>
+        <div className='text-white text-center text-xs pb-3'>© 2024 Marcos Jianoto. All rights reserved.</div>
       </div>
 
 
