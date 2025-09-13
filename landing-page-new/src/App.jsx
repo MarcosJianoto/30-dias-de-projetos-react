7895504ffe76aa193b587dc40f1207be395c9ccc
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-13 15:25:10 -0300

    okay

--- landing-page-new/src/App.jsx
+++ landing-page-new/src/App.jsx
@@ -4,10 +4,22 @@ function App() {
 
   return (
 
-    <div className="flex flex-col bg-gray-800 text-white min-h-screen" >
+    <div className="flex bg-gray-950 " >
 
+      <div className='flex flex-col justify-center align-center text-white min-h-screen mx-auto'>
+        <div className=' text-sm items-start mx-20  '>
+          i am
+        </div>
+        <div className='font-black text-7xl items-start my-5'>
+          MARCOS JIANOTO
+        </div>
+        <div className="flex flex-col text-sm mt-2 items-end mx-20">
+          <div>full stack</div>
+          <div>developer</div>
+        </div>
+      </div>
 
-    </div>
+    </div >
 
   )
 }
