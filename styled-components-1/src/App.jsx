5fe7731f566d60534f358636f459661bc5e7b496
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-13 19:21:51 -0300

    Class 1 Alura - Done

--- styled-components-1/src/App.jsx
+++ styled-components-1/src/App.jsx
@@ -1,13 +1,23 @@
+import { styled } from "styled-components"
+import GlobalsStyles from "./components/GlobalsStyles/index.jsx"
+
+
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100vh;
+  min-height: 100vh;
+`
+
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Styled Components</h1>
-        <p>Styled Components é uma biblioteca que permite escrever CSS dentro do JavaScript, utilizando a técnica de CSS-in-JS. Ela é amplamente utilizada em projetos React para estilizar componentes de forma modular e reutilizável.</p>
-        <p>Com Styled Components, você pode criar componentes estilizados que encapsulam tanto a lógica quanto o estilo, facilitando a manutenção e o reaproveitamento do código. A sintaxe utiliza template literals do JavaScript para definir estilos CSS diretamente dentro dos componentes.</p>
-        <p>Além disso, Styled Components oferece recursos avançados, como theming (tematização), suporte a props para estilos dinâmicos, e a capacidade de criar componentes com estilos condicionais. Isso torna a biblioteca uma escolha popular para desenvolvedores que buscam uma abordagem moderna e eficiente para estilizar aplicações React.</p>
-      </div>
+      <FundoGradiente>
+        <GlobalsStyles />
+
+
+      </FundoGradiente>
     </>
   )
 }
