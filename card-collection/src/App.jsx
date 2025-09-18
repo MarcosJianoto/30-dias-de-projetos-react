f84ca8e800557e1e7d16d6dc50586a7507a24fa6
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-18 17:04:15 -0300

    Quad teste incomplete

--- card-collection/src/App.jsx
+++ card-collection/src/App.jsx
@@ -1,6 +1,8 @@
 import { styled } from "styled-components"
 import GlobalsStyles from "../../styled-components-2/src/components/GlobalsStyles"
 import Card from "./components/Card"
+import { FaBeer, FaHome, FaUser, FaGithub } from 'react-icons/fa';
+import { MdAlarm, MdFavorite } from 'react-icons/md';
 
 
 const FundoGradiente = styled.div`
@@ -8,11 +10,22 @@ const FundoGradiente = styled.div`
   width: 100vw;
   min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center; 
-  gap: 40px;
-  display: flex;
+  justify-content: center; 
+  gap: 20px;
+  flex-wrap: wrap;
+  align-items: center;
+`
 
+const QuadMenor = styled.div`
+  width: 80vw;
+  height: 60vh;
+  border: 1px solid white;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: center;
 `
 
 function App() {
@@ -21,7 +34,17 @@ function App() {
     <>
       <FundoGradiente>
         <GlobalsStyles />
-        <Card />
+        <QuadMenor>
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Build your online store"} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Facebook & Google ads"} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Attract more clients"} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Premium design & visuals"} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Words that sell"} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Content for your brand"} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Grow your business online."} />
+          <Card image={< FaHome size={30} color="white" />} title={"e-Commerce"} text={"Turn more visitors into customers."} />
+
+        </QuadMenor>
 
       </FundoGradiente >
     </>
--- card-collection/src/components/Card/index.jsx
+++ card-collection/src/components/Card/index.jsx
@@ -1,29 +1,54 @@
 import styled from "styled-components"
 
 const ContainerCard = styled.div`
-    width: 400px;
+    width: calc(60% - 20%);
+    max-width: 400px;
     height: 80px;
+    min-height: 80px;
     border: 1px solid white;
     border-radius: 15px;
     align-items: center;
-    justify-content: center;
     display: flex;
 `
 
 const ContainerIntern = styled.div`
-    width: 60px;
-    height: 60px;
+    
+    width: 50px;
+    height: 50px;
     border: 1px solid white;
     border-radius: 15px;
-    text-align: center;
-    justify-content: center;    
-    
+    display: flex;
+    align-items: center;
+    justify-content: center;   
+    position: relative;
+    left: 10px;    
 `
 
-const Card = () => {
+const ContainerText = styled.div`
+
+    width: 200px;
+    height: 50px;
+    border: 1px solid white;    
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    left: 15px;
+    color: white;
+    padding: 5px;
+`
+
+const Card = ({ image, title, text }) => {
     return (
         <ContainerCard>
-            <ContainerIntern />
+            <ContainerIntern>
+                {image}
+            </ContainerIntern>
+            <ContainerText>
+                {title}
+                {text}
+            </ContainerText>
         </ContainerCard>
     )
 }
