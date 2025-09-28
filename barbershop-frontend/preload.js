ade58953794f4b89f7e0d4608331e524a7730ae8
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-09-28 08:45:30 -0300

    init barbersop-frontend in electron + gvite

---
+++ barbershop-frontend/preload.js
@@ -0,0 +1,8 @@
+import { contextBridge } from 'electron';
+import process from 'process';
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+});
