0b16bf506477a1b8414574b611565f0851ead715
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-10-16 21:29:05 -0300

    Products add new product and export

--- barbershop-frontend/src/components/ProductsTable/MenuProduct/index.jsx
+++ barbershop-frontend/src/components/ProductsTable/MenuProduct/index.jsx
@@ -1,31 +1,24 @@
 import { Box, Typography, Button } from "@mui/material"
 import AddIcon from "@mui/icons-material/Add";
-import { IconButton } from "@mui/material";
-
-import styled from "styled-components"
-
-const ContainerMain = styled.div`
-    display: flex;
-    flex-direction: column;
-    border-radius: 5px;
-    height: 15%;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid red;
-    padding: 15px;
-    `
 
 const MenuProduct = () => {
     return (
         <>
             <Box
                 display={'flex'}
-                justifyContent={'center'}
+                justifyContent={'space-between'}
                 alignItems={'center'}
+                width={'100%'}
             >
                 <Typography variant="h4"> Product </Typography>
-                <Box>
+                <Box
+                    display={'flex'}
+                    justifyContent={'space-between'}
+                    gap={'20px'}
+                >
+                    <Button variant="outlined" startIcon={<AddIcon />}>
+                        Export
+                    </Button>
                     <Button variant="contained" startIcon={<AddIcon />}>
                         Adicionar novo produto
                     </Button>
--- barbershop-frontend/src/pages/Products.jsx
+++ barbershop-frontend/src/pages/Products.jsx
@@ -10,20 +10,7 @@ const ContainerMain = styled.div`
     width: 100%;
     justify-content: space-between;
     align-items: center;
-    border: 1px solid red;
-    padding: 15px;
     `
-
-const ContainerMenuProduct = styled.div`
-    display: flex;
-    border-radius: 5px;
-    height: 15%;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid red;
-    padding: 15px;
-`
 
 const Products = () => {
     return (
