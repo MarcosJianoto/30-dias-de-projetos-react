0b471325f9dfb127040f7340ba16a969115dd1ed
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-11-26 22:46:03 -0300

    primeiro ex de lógica, utilizando arr.filter()

---
+++ ex-logica-treino/1.js
@@ -0,0 +1,30 @@
+const arr = [
+  {
+    nome: "Pedro",
+    email: "pedro@teste.com",
+    ativo: true,
+  },
+  {
+    nome: "Marcos",
+    email: "marcos@teste.com",
+    ativo: false,
+  },
+  {
+    nome: "Ana",
+    email: "ana@teste.com",
+    ativo: true,
+  },
+  {
+    nome: "Carla",
+    email: "carla@teste.com",
+    ativo: false,
+  },
+  {
+    nome: "João",
+    email: "joao@teste.com",
+    ativo: true,
+  },
+];
+
+const ativos = arr.filter((user) => user.ativo === true);
+console.log(ativos);
