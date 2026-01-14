33fec33d7238e57a617558c3e2db0630f232fefe
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2026-01-14 17:11:57 -0300

    thats ok

---
+++ ex-logica-treino/1-30/23.js
@@ -0,0 +1,60 @@
+const saudacao = () => {
+  console.log("Olá, seja bem vindo!");
+};
+
+saudacao();
+
+const apresentarPessoa = (nome = "Marcos", idade = 15) => {
+  console.log("Ola, meu nome é ", nome, "e tenho", idade, "anos");
+};
+
+apresentarPessoa();
+
+//calculo de IMC
+const calcularIMC = (peso, altura) => {
+  return peso / (altura * altura);
+};
+
+console.log(calcularIMC(78, 1.78));
+
+//verificar aprovação
+const verificarAprovacao = (nota) => {
+  if (nota >= 7) {
+    console.log("passou");
+  } else {
+    console.log("reprovado");
+  }
+};
+
+verificarAprovacao(8);
+
+const ehPar = (n) => {
+  if (n % 2 == 0) {
+    console.log("numero", n, "é Par");
+  } else {
+    console.log("numero", n, "n é Par");
+  }
+};
+
+ehPar(5);
+
+//fazer pergunta
+const respostaParaSerCorreta = "sim";
+const fazerPergunta = (pergunta, respostaCorreta) => {
+  if (respostaCorreta === respostaParaSerCorreta) {
+    console.log(pergunta);
+  }
+};
+fazerPergunta("precisa?", "sim");
+
+//callback => funcao recebe outra função como argumento.
+const nameW = (name) => {
+  return console.log(name);
+};
+
+const functionWithCallback = (calback) => {
+  let name = "marcos";
+  return calback(name);
+};
+
+functionWithCallback(nameW);
